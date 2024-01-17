182c4f7f91821ba8865ad0f38d4e97b9c5fafd16
Linus Romland <hello@linusromland.com>
2024-01-17 17:58:50 +0100

    Initialize of dotnet and DSharpPlus
    Co-Authored-By: Markus Simonsen <33492046+markussim@users.noreply.github.com>

---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿using dotenv.net;
+using DSharpPlus;
+
+namespace DiscordSoundboard
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			DotEnv.Load();
+			IDictionary<string, string> envVars = DotEnv.Read();
+
+			var discord = new DiscordClient(new DiscordConfiguration()
+			{
+				Token = envVars["DISCORD_TOKEN"],
+				TokenType = TokenType.Bot,
+				Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+			});
+
+			discord.MessageCreated += async (s, e) =>
+			{
+				if (e.Message.Content.ToLower().StartsWith("ping"))
+					await e.Message.RespondAsync("pong!");
+			};
+
+			await discord.ConnectAsync();
+			await Task.Delay(-1);
+		}
+	}
+}
